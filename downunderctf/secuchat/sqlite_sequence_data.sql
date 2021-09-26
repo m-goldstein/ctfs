5d8021eaaba0448646283ee5d66083bdc6401cac
max goldstein <mgoldstein1998@gmail.com>
2021-09-25 20:55:39 -0500

    .

---
+++ downunderctf/secuchat/sqlite_sequence_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO "table" VALUES('Parameters',333);
+INSERT INTO "table" VALUES('Conversation',46);
+Conversation  Message       Parameters    User        
+Conversation  Message       Parameters    User        
